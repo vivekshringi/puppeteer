6976b22832cc164a930147547b0eb86d7a982f8b
vivekshringi <shringi.vivek@gmail.com>
2020-05-31 12:49:40 +0200

    added progress of $$

--- src/example.test.ts
+++ src/example.test.ts
@@ -1,18 +1,14 @@
-import puppeteer from 'puppeteer';
-import {
-  openBrowser,
-  openPage,
-  textBySelector,
-} from './helper';
+import puppeteer from "puppeteer";
+import { openBrowser, openPage, textBySelector } from "./helper";
 
 let browser: puppeteer.Browser;
 let page: puppeteer.Page;
-const pageName = '/';
+const pageName = "/";
 let viewport = {
   width: 1280,
   height: 980,
-  deviceScaleFactor: 1
-}
+  deviceScaleFactor: 1,
+};
 
 beforeEach(async () => {
   browser = await openBrowser();
@@ -26,48 +22,62 @@ afterEach(async () => {
   }
 });
 
-describe('Example Test Scenarios', () => {
-  test('check if page title is correct', async () => {
+describe("Example Test Scenarios", () => {
+  test("check if page title is correct", async () => {
     const title = await page.title();
-    expect(title).toEqual('The Internet');
-})
-
-test('check if basic authentication works', async () => {
-  await page.setExtraHTTPHeaders({
-    Authorization: `Basic ${Buffer.from('admin:admin').toString('base64')}`
+    expect(title).toEqual("The Internet");
   });
-  await page.goto((await page.url())+'/basic_auth')
-  const successMessage = await textBySelector(page, '.example p');
-  expect(successMessage.trim()).toEqual("Congratulations! You must have the proper credentials.")
-})
 
-test('check if basic authentication works using page.authenticate  ', async () => {
-  await page.authenticate({username:"admin",
-password:"admin"});
-  await page.goto((await page.url())+'/basic_auth')
-  const successMessage = await textBySelector(page, '.example p');
-  expect(successMessage.trim()).toEqual("Congratulations! You must have the proper credentials.")
-})
-
-test('check if window width and height is correct on opening', async () => {
-  const dimensions = await page.evaluate(() => {
-    return {
-      width: document.documentElement.clientWidth,
-      height: document.documentElement.clientHeight,
-      deviceScaleFactor: window.devicePixelRatio
-    };
+  test("check if basic authentication works", async () => {
+    await page.setExtraHTTPHeaders({
+      Authorization: `Basic ${Buffer.from("admin:admin").toString("base64")}`,
+    });
+    await page.goto((await page.url()) + "/basic_auth");
+    const successMessage = await textBySelector(page, ".example p");
+    expect(successMessage.trim()).toEqual(
+      "Congratulations! You must have the proper credentials."
+    );
   });
-   expect(dimensions).toEqual(viewport);
-});
 
-test('check $ selector use', async () => {
-  await page.goto('https://the-internet.herokuapp.com')
-  const elementHandle = await page.$('h1');
-  const propertyDetails = await elementHandle?.getProperty('innerText');
-  const location = await elementHandle?.boundingBox();
-  await elementHandle?.screenshot({'path':'hello.jpg'});
-  const innerText = await propertyDetails?.jsonValue()
-  expect(innerText).toEqual('Welcome to the-internet')
-  expect(location).toEqual({ x: 155, y: 40.796875, width: 970, height: 61 });
-});
+  test("check if basic authentication works using page.authenticate  ", async () => {
+    await page.authenticate({ username: "admin", password: "admin" });
+    await page.goto((await page.url()) + "/basic_auth");
+    const successMessage = await textBySelector(page, ".example p");
+    expect(successMessage.trim()).toEqual(
+      "Congratulations! You must have the proper credentials."
+    );
+  });
+
+  test("check if window width and height is correct on opening", async () => {
+    const dimensions = await page.evaluate(() => {
+      return {
+        width: document.documentElement.clientWidth,
+        height: document.documentElement.clientHeight,
+        deviceScaleFactor: window.devicePixelRatio,
+      };
+    });
+    expect(dimensions).toEqual(viewport);
+  });
+
+  test("check $ selector use", async () => {
+    await page.goto("https://the-internet.herokuapp.com");
+    const elementHandle = await page.$("h1");
+    const propertyDetails = await elementHandle?.getProperty("innerText");
+    const location = await elementHandle?.boundingBox();
+    await elementHandle?.screenshot({ path: "hello.jpg" });
+    const innerText = await propertyDetails?.jsonValue();
+    expect(innerText).toEqual("Welcome to the-internet");
+    expect(location).toEqual({ x: 155, y: 40.796875, width: 970, height: 61 });
+  });
+
+  test("check $$ selector use", async () => {
+    await page.goto("https://the-internet.herokuapp.com");
+    const elementHandles = await page.$$("li>a");
+    console.log(elementHandles.length);
+    for(let i =0; i<elementHandles.length;i++){
+    const value = await elementHandles[i]?.getProperty("href");
+    const href = await value?.jsonValue();
+    console.log(href);
+    }
+  });
 });
