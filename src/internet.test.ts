e40ec32ed23409fdb0493d5a9fa2583e0b6d2275
vivekshringi <shringi.vivek@gmail.com>
2020-11-03 22:56:23 +0100

    added authentication scenarios

--- src/internet.test.ts
+++ src/internet.test.ts
@@ -9,27 +9,34 @@ const e = {
   deleteButton: 'button[onclick*="deleteElement()"]',
   elementSelenium: 'div>a[href*="ele"]',
   forkElement: 'a[href*="github"]',
+  successMessage: ".example p",
 };
 const d = {
   expHeader: "Add/Remove Elements",
   expLinkedURL: "http://elementalselenium.com/",
+  expSuccessMessage: "Congratulations! You must have the proper credentials.",
 };
 
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    headless: false,
+  });
+  [page] = await browser.pages();
+});
+
 afterAll(async () => {
-  await page.close();
   await browser.close();
 });
 
 describe("The Internet Scenarios", () => {
   describe("The Add/Remove element scenarios", () => {
     beforeAll(async () => {
-      browser = await puppeteer.launch({
-        headless: true
-      });
-      [page] = await browser.pages();
       await page.goto(`${targetURL}add_remove_elements/`, {
         waitUntil: "networkidle0",
       });
+    });
+    afterAll(async () => {
+      await page.close();
     });
     test("Verify opening add/remove elements", async () => {
       expect(await page.url()).toContain("add_remove_elements/");
@@ -77,9 +84,9 @@ describe("The Internet Scenarios", () => {
 
     test("Verify element size and dimensions", async () => {
       const elemetSize = await (await page.$(e.addButton))?.boundingBox();
-      expect(elemetSize?.height).toContain(45);
-      expect(elemetSize?.width).toContain(147);
-      expect(elemetSize?.y).toContain(103);
+      expect(elemetSize?.height).toEqual(46);
+      expect(elemetSize?.width).toEqual(147.453125);
+      expect(elemetSize?.y).toEqual(103.890625);
       expect(elemetSize?.x).toEqual(15);
     });
 
@@ -118,6 +125,53 @@ describe("The Internet Scenarios", () => {
         }
         await page.waitFor(300);
         console.log(`waiting for ${count} times`);
+      }
+    });
+  });
+  describe("Authentication scenarios", () => {
+    let context: puppeteer.BrowserContext;
+    beforeEach(async () => {
+      context = await browser.createIncognitoBrowserContext();
+      page = await context.newPage();
+    });
+
+    afterEach(async () => {
+      await page.close();
+      await context?.close();
+    });
+
+    const login = async () => {
+      await page.goto(`${targetURL}/basic_auth`, { waitUntil: "networkidle0" });
+      const successMessage = await page.$eval(
+        e.successMessage,
+        (el) => el.textContent
+      );
+      expect(successMessage?.trim()).toEqual(d.expSuccessMessage);
+    };
+
+    test("check if basic authentication works", async () => {
+      await page.setExtraHTTPHeaders({
+        Authorization: `Basic ${Buffer.from("admin:admin").toString("base64")}`,
+      });
+      await login();
+    });
+
+    test("check if basic authentication works using page.authenticate  ", async () => {
+      await page.authenticate({ username: "admin", password: "admin" });
+      await login();
+    });
+
+    test("check on clicking on cancel unauthorized message is displayed  ", async () => {
+      page.once("response", (response) => {
+        expect(response.status()).toEqual(401);
+        expect(response.statusText()).toEqual("Unauthorized");
+      });
+      try {
+        await page.goto(`${targetURL}/basic_auth`, {
+          waitUntil: "networkidle0",
+        });
+      } catch (error) {
+        expect(error.message).toContain("net::ERR_INVALID_AUTH_CREDENTIALS");
       }
     });
   });
